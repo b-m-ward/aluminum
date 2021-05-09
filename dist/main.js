05cf07929179fc56a502c5aa5e5a9feacd93719f
b-m-ward <bbmmward@protonmail.com>
2021-05-09 13:51:21 -0400

    basic structure in place to open multiple urls

--- dist/main.js
+++ dist/main.js
@@ -3,3 +3,30 @@
 console.log('main loaded');
 chrome.browserAction.setBadgeBackgroundColor({ color: '#313131' });
 chrome.browserAction.setBadgeText({ text: "Al!" });
+var button_ref = document.getElementById('search-sources');
+var ticker_ref = document.getElementById('ticker');
+function search() {
+    var val = ticker_ref["value"];
+    console.log('searching: ', val);
+    var urls = getUrls(val);
+    for (var i = 0; i < urls.length; i++) {
+        // will open each link in the current window
+        chrome.tabs.create({
+            url: urls[i]
+        });
+    }
+}
+function getUrls(ticker) {
+    var urls = [
+        "https://finance.yahoo.com/quote/" + ticker,
+        "https://www.nasdaq.com/market-activity/stocks/" + ticker,
+        "https://seekingalpha.com/symbol/" + ticker,
+        "http://openinsider.com/search?q=" + ticker
+    ];
+    return urls;
+}
+function setup() {
+    console.log('setting up handlers');
+    button_ref.addEventListener("click", search);
+}
+setup();
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -4,3 +4,37 @@
 console.log('main loaded');
 chrome.browserAction.setBadgeBackgroundColor({color: '#313131'});
 chrome.browserAction.setBadgeText({ text: "Al!" });
+
+const button_ref = document.getElementById('search-sources');
+const ticker_ref = document.getElementById('ticker');
+
+function search() : void {
+    let val = ticker_ref["value"];
+    console.log('searching: ', val);
+    let urls = getUrls(val);
+    for (var i = 0; i < urls.length; i++) {
+        // will open each link in the current window
+        chrome.tabs.create({
+            url: urls[i]
+        });
+    }
+}
+
+function getUrls(ticker: string) : string[] {
+    let urls = [] = [
+        `https://finance.yahoo.com/quote/${ticker}`,
+        `https://www.nasdaq.com/market-activity/stocks/${ticker}`,
+        `https://seekingalpha.com/symbol/${ticker}`,
+        `http://openinsider.com/search?q=${ticker}`
+    ];
+    return urls;
+}
+
+function setup() : void {
+    console.log('setting up handlers');
+    button_ref.addEventListener("click", search);
+}
+
+
+setup();
+
