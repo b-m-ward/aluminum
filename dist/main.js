f9e0513110bcc3a2008508d45811cb681cd55658
b-m-ward <bryantmward@gmail.com>
2021-05-09 19:41:47 -0400

    structure updates

--- dist/main.js
+++ dist/main.js
@@ -1,35 +1,46 @@
-//ALL other ts files should import to here
-//The build script will pull them in to output the main.js file
-console.log('main loaded');
 chrome.browserAction.setBadgeBackgroundColor({ color: '#313131' });
 chrome.browserAction.setBadgeText({ text: "Al!" });
 var button_ref = document.getElementById('search-sources');
 var ticker_ref = document.getElementById('ticker');
+var urls = [
+    { url: "https://finance.yahoo.com/quote/", isSearchable: true, prettyName: 'yahoo finance' },
+    { url: "https://www.nasdaq.com/market-activity/stocks/", isSearchable: true, prettyName: 'nasdaq' },
+    { url: "https://seekingalpha.com/symbol/", isSearchable: true, prettyName: 'seeking alpha' },
+    { url: "http://openinsider.com/search?q=", isSearchable: true, prettyName: 'open insider' },
+    { url: "https://www.sec.gov/edgar/search/#/entityName=", isSearchable: true, prettyName: 'sec EDGAR' },
+    { url: "https://stockcharts.com/h-sc/ui?s=", isSearchable: true, prettyName: 'stockcharts' },
+    { url: "https://robinhood.com/stocks/", isSearchable: true, prettyName: 'robinhood' },
+    { url: "https://www.moodys.com/search?keyword=", isSearchable: true, prettyName: 'moodys' }
+];
 function search() {
     var val = ticker_ref["value"];
-    console.log('searching: ', val);
-    var urls = getUrls(val);
-    for (var i = 0; i < urls.length; i++) {
+    var searchables = urls.filter(function (r) { return r.isSearchable === true; });
+    for (var i = 0; i < searchables.length; i++) {
         // will open each link in the current window
         chrome.tabs.create({
-            url: urls[i]
+            url: urls[i].url + val
         });
     }
 }
 function getUrls(ticker) {
     var urls = [
-        "https://finance.yahoo.com/quote/" + ticker,
-        "https://www.nasdaq.com/market-activity/stocks/" + ticker,
-        "https://seekingalpha.com/symbol/" + ticker,
-        "http://openinsider.com/search?q=" + ticker,
-        "https://www.sec.gov/edgar/search/#/entityName=" + ticker,
-        "https://stockcharts.com/h-sc/ui?s=" + ticker,
-        "https://robinhood.com/stocks/" + ticker
+        { url: "https://finance.yahoo.com/quote/" + ticker, isSearchable: true, prettyName: 'yahoo finance' },
+        { url: "https://www.nasdaq.com/market-activity/stocks/" + ticker, isSearchable: true, prettyName: 'nasdaq' },
+        { url: "https://seekingalpha.com/symbol/" + ticker, isSearchable: true, prettyName: 'seeking alpha' },
+        { url: "http://openinsider.com/search?q=" + ticker, isSearchable: true, prettyName: 'open insider' },
+        { url: "https://www.sec.gov/edgar/search/#/entityName=" + ticker, isSearchable: true, prettyName: 'sec EDGAR' },
+        { url: "https://stockcharts.com/h-sc/ui?s=" + ticker, isSearchable: true, prettyName: 'stockcharts' },
+        { url: "https://robinhood.com/stocks/" + ticker, isSearchable: true, prettyName: 'robinhood' },
+        { url: "https://www.moodys.com/search?keyword=" + ticker, isSearchable: true, prettyName: 'moodys' }
     ];
     return urls;
 }
 function setup() {
-    console.log('setting up handlers');
     button_ref.addEventListener("click", search);
 }
 setup();
+var SearchSource = /** @class */ (function () {
+    function SearchSource() {
+    }
+    return SearchSource;
+}());
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,34 +1,41 @@
-//ALL other ts files should import to here
-//The build script will pull them in to output the main.js file
-
 chrome.browserAction.setBadgeBackgroundColor({color: '#313131'});
 chrome.browserAction.setBadgeText({ text: "Al!" });
 
 const button_ref = document.getElementById('search-sources');
 const ticker_ref = document.getElementById('ticker');
 
+var urls: SearchSource[] = [
+    { url: `https://finance.yahoo.com/quote/`, isSearchable: true, prettyName: 'yahoo finance' },
+    { url: `https://www.nasdaq.com/market-activity/stocks/`, isSearchable: true, prettyName: 'nasdaq'},
+    { url: `https://seekingalpha.com/symbol/`, isSearchable: true, prettyName: 'seeking alpha'},
+    { url: `http://openinsider.com/search?q=`, isSearchable: true, prettyName: 'open insider'},
+    { url: `https://www.sec.gov/edgar/search/#/entityName=`, isSearchable: true, prettyName: 'sec EDGAR'},
+    { url: `https://stockcharts.com/h-sc/ui?s=`, isSearchable: true, prettyName: 'stockcharts'},
+    { url: `https://robinhood.com/stocks/`, isSearchable: true, prettyName: 'robinhood'},
+    { url: `https://www.moodys.com/search?keyword=`, isSearchable: true, prettyName: 'moodys' }
+];
+
 function search() : void {
     let val = ticker_ref["value"];
-    console.log('searching: ', val);
-    let urls = getUrls(val);
-    for (var i = 0; i < urls.length; i++) {
+    let searchables = urls.filter(r => { return r.isSearchable === true });
+    for (var i = 0; i < searchables.length; i++) {
         // will open each link in the current window
         chrome.tabs.create({
-            url: urls[i]
+            url: urls[i].url + val
         });
     }
 }
 
-function getUrls(ticker: string) : string[] {
-    let urls = [] = [
-        `https://finance.yahoo.com/quote/${ticker}`,
-        `https://www.nasdaq.com/market-activity/stocks/${ticker}`,
-        `https://seekingalpha.com/symbol/${ticker}`,
-        `http://openinsider.com/search?q=${ticker}`,
-        `https://www.sec.gov/edgar/search/#/entityName=${ticker}`,
-        `https://stockcharts.com/h-sc/ui?s=${ticker}`,
-        `https://robinhood.com/stocks/${ticker}`,
-        `https://www.moodys.com/search?keyword=${ticker}`
+function getUrls(ticker: string) : SearchSource[] {
+    let urls: SearchSource[] = [
+        { url: `https://finance.yahoo.com/quote/${ticker}`, isSearchable: true, prettyName: 'yahoo finance' },
+        { url: `https://www.nasdaq.com/market-activity/stocks/${ticker}`, isSearchable: true, prettyName: 'nasdaq'},
+        { url: `https://seekingalpha.com/symbol/${ticker}`, isSearchable: true, prettyName: 'seeking alpha'},
+        { url: `http://openinsider.com/search?q=${ticker}`, isSearchable: true, prettyName: 'open insider'},
+        { url: `https://www.sec.gov/edgar/search/#/entityName=${ticker}`, isSearchable: true, prettyName: 'sec EDGAR'},
+        { url: `https://stockcharts.com/h-sc/ui?s=${ticker}`, isSearchable: true, prettyName: 'stockcharts'},
+        { url: `https://robinhood.com/stocks/${ticker}`, isSearchable: true, prettyName: 'robinhood'},
+        { url: `https://www.moodys.com/search?keyword=${ticker}`, isSearchable: true, prettyName: 'moodys' }
     ];
     return urls;
 }
@@ -38,3 +45,9 @@ function setup() : void {
 }
 
 setup();
+
+class SearchSource {
+    url: string;
+    isSearchable: boolean;
+    prettyName: string;
+}
